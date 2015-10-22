c4bd15935305c2b679c833d3764dd252d905aaf4
Steve Gengler <steve@o3world.com>
2015-10-22 09:46:35 -0400

    update demos

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 (function($) {
 
+//setting up menu on/off class
 
   $('#burger').on('click', function(e) {
     e.preventDefault();
     $('body').toggleClass('menu-open');
   })
 
+  //for animated burger
   $('.burger-menu').on('click', function() {
     var el = $(this);
     el.toggleClass('burger-active');
@@ -19,20 +21,23 @@
   });
 
 
+//Old school way of detecting load
+
   var bgLoadDetect = function() {
     var el = $(this),
       src = el.css('background-image').replace(/(^url\()|(\)$|[\"\'])/g, '');
     $('<img/>').one('load', function() {
-      console.log('Load Check 1')
+      // Image has loaded;
     }).attr('src', src);
   }
-
   $(".js--load-check").each(bgLoadDetect);
+
+
+//Plugins make it easer :)
 
   $(".js--load-check").waitForImages(true).done(function() {
     $(this).addClass('loaded');
   });
 
-  $('body').addClass('active');
 
 })(jQuery);
